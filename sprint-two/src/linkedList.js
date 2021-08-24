a5ff6476d29e511f50be383bd589c52bcd1516f9
Othniel Chan <othnielhr.chan@gmail.com>
2021-08-23 22:33:59 -0500

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -8,19 +8,38 @@ var LinkedList = function() {
     if (list.head === null) {
       list.head = newNode;
       list.tail = newNode;
+    } else {
+      var currentNode = list.head;
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      }
+
+      currentNode.next = newNode;
+      list.tail = newNode;
     }
-    var currentNode = list.head;
-    while (currentNode.next !== null) {
-      currentNode = currentNode.next;
-    }
-    currentNode.next = newNode;
-    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var temp = list.head.value;
+    if (list.head.next !== null) {
+      list.head = list.head.next;
+    }
+    return temp;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode.next !== null) {
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    if (currentNode.value === target) {
+      return true;
+    }
+    return false;
   };
 
   return list;
