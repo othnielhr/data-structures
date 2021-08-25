7959edc88599d017a5f97a2eaf46bbabbf7c7b9f
Othniel Chan <othnielhr.chan@gmail.com>
2021-08-25 14:20:54 -0500

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,52 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  var isPresent = false;
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      isPresent = true;
+    }
+  }
+  if (!isPresent) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  var isPresent = false;
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      isPresent = true;
+    }
+  }
+  return isPresent;
 };
 
 setPrototype.remove = function(item) {
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      if (i === this._storage.length - 1) {
+        this._storage.pop();
+      } else {
+        for (var j = i; j < this._storage.length - 1; j++) {
+          this._storage[j] = this._storage[j + 1];
+        }
+        this._storage.pop();
+      }
+    }
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+time complexity for add() = linear time O(n);
+time complexity for contains() = linear time O(n);
+time complexity for remove() = linear time O(n);
+
  */
