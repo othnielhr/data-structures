178f1e60247b0c1fab3707c26faf1dc0fbf454a8
Othniel Chan <othnielhr.chan@gmail.com>
2021-08-26 19:57:26 -0500

    Complete Hash Table

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,52 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = []; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  var isPresent = false;
-  for (var i = 0; i < this._storage.length; i++) {
-    if (this._storage[i] === item) {
-      isPresent = true;
-    }
-  }
-  if (!isPresent) {
-    this._storage.push(item);
-  }
+  this._storage[item] = item;
 };
 
+
 setPrototype.contains = function(item) {
-  var isPresent = false;
-  for (var i = 0; i < this._storage.length; i++) {
-    if (this._storage[i] === item) {
-      isPresent = true;
-    }
-  }
-  return isPresent;
+  if (this._storage[item]) {
+    return true;
+  } return false;
 };
 
 setPrototype.remove = function(item) {
-  for (var i = 0; i < this._storage.length; i++) {
-    if (this._storage[i] === item) {
-      if (i === this._storage.length - 1) {
-        this._storage.pop();
-      } else {
-        for (var j = i; j < this._storage.length - 1; j++) {
-          this._storage[j] = this._storage[j + 1];
-        }
-        this._storage.pop();
-      }
-    }
+  if (this._storage[item]) {
+    delete this._storage[item];
   }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
-time complexity for add() = linear time O(n);
-time complexity for contains() = linear time O(n);
-time complexity for remove() = linear time O(n);
-
+time complexity for add() = constant time O(1);
+time complexity for contains() = constant time O(1);
+time complexity for remove() = constant time O(1); 
  */
